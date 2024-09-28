47465b7efddfb025d7b4f36000b0f376f527c287
MenfTours <menftourss@gmail.com>
2024-09-28 05:26:36 +0300

    HOT FIXES

--- app/(form)/customBooking.jsx
+++ app/(form)/customBooking.jsx
@@ -39,7 +39,7 @@ export default function CustomBooking( {isOpen, onClose} ) {
                         <form className="p-4 md:p-5" method="GET"  id="form-custom-booking">
                             <label for="CUSTOM-FROM" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destination from</label>
                             <div class="flex">
-                                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md main-icon-grey-- main-grey-- dark:border-gray-600">
                                 <i class="ph-fill ph-airplane-takeoff" style={{fontSize:'1.1rem'}}></i>
                                 </span>
                                 <input type="text" id="CUSTOM-FROM" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  placeholder="Egypt" required />
@@ -47,7 +47,7 @@ export default function CustomBooking( {isOpen, onClose} ) {
                                 
                             <label for="CUSTOM-TO" style={{marginTop:'10px'}} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destination to</label>
                             <div class="flex" >
-                                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md main-icon-grey-- main-grey-- dark:border-gray-600">
                                 <i class="ph-fill ph-airplane-landing" style={{fontSize:'1.1rem'}}></i>
                                  </span>
                                 <input type="text" id="CUSTOM-TO" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Dubai" />
@@ -59,7 +59,7 @@ export default function CustomBooking( {isOpen, onClose} ) {
 
                                     <label for="DATE-FROM" style={{marginTop:'10px'}} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date from</label>
                                     <div className="flex" >
-                                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md  main-icon-grey-- main-grey-- dark:border-gray-600">
                                         <i class="ph-fill ph-calendar-blank" style={{fontSize:'1.1rem'}}></i>
                                         </span>
                                         <input type="date" min={new Date().toISOString().split('T')[0]} id="DATE-FROM" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required   />
@@ -70,7 +70,7 @@ export default function CustomBooking( {isOpen, onClose} ) {
 
                                     <label for="DATE-TO" style={{marginTop:'10px'}} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date to</label>
                                     <div className="flex"   >
-                                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md main-icon-grey-- main-grey-- dark:border-gray-600">
                                         <i  class="ph-fill ph-calendar-blank" style={{fontSize:'1.1rem'}}></i>
                                         </span>
                                         <input type="date" min={new Date().toISOString().split('T')[0]} id="DATE-TO" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required  />
